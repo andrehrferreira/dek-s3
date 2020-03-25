f8b8aeb8db5305cd70ccc0e58518855d47f0164b
Andre Ferreira <andrehrf@gmail.com>
2020-03-25 09:52:16 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# s3
-Amazon S3 interface plugin for DEK
+# @dekproject/s3
+
+AWS S3 interface plugin for DEK
+
+What does this plugin do?
+
+* Control configuration for connection to AWS S3 in production development mode in a simplified way with **dotenv**
+* Performs connection implementation along the lines ES6 being pre requirement to start the project
+
+## Instalation
+
+To install the bootstrap we recommend using the CLI
+
+```bash
+$ yarn add @dekproject/s3 --save
+$ nano .env
+```
+
+In the .env file add the following settings
+
+```
+S3_BUCKET_NAME=bucket-name
+S3_TMP_UPLOAD_DIRETORY=./
+
+AWS_ACCESS_KEY_ID=AWS_KEY
+AWS_SECRET_ACCESS_KEY=AWS_SECRET
+AWS_DEFAULT_REGION=AWS_REGION
+```
+
+## Usage
+
+Using direct
+
+```bash
+$ npm i @dekproject/scope
+```
+
+Using in the standard DEK skeleton
+
+```js
+import { $, app, s3 } from "@dekproject/scope";
+
+app.get("/upload", s3.single("file"), (req, res) => {
+    const {
+        originalname: name,
+        size,
+        key,
+        location: url = ""
+    } = req.file;
+
+    console.log(url);//https://mybucket.s3.amazonaws.com/as12d31as23d12a3s1da3s2d
+});
+
+$.wait("s3").then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`);
+    });
+});
+```
---
+++ build/index.js
@@ -0,0 +1,89 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _multer = require("multer");
+
+var _multer2 = _interopRequireDefault(_multer);
+
+var _path = require("path");
+
+var _path2 = _interopRequireDefault(_path);
+
+var _crypto = require("crypto");
+
+var _crypto2 = _interopRequireDefault(_crypto);
+
+var _awsSdk = require("aws-sdk");
+
+var _awsSdk2 = _interopRequireDefault(_awsSdk);
+
+var _multerS = require("multer-s3");
+
+var _multerS2 = _interopRequireDefault(_multerS);
+
+var _scope = require("@dekproject/scope");
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
+exports.default = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
+    return regeneratorRuntime.wrap(function _callee$(_context) {
+        while (1) {
+            switch (_context.prev = _context.next) {
+                case 0:
+                    try {
+                        if (!Object.prototype.hasOwnProperty.call(process.env, "S3_TMP_UPLOAD_DIRETORY")) {
+                            // eslint-disable-next-line no-console
+                            console.log("[ S3 ] - There is no S3_TMP_UPLOAD_DIRETORY variable in the .env file.");
+                        } else if (!Object.prototype.hasOwnProperty.call(process.env, "S3_BUCKET_NAME")) {
+                            // eslint-disable-next-line no-console
+                            console.log("[ S3 ] - There is no S3_BUCKET_NAME variable in the .env file.");
+                        } else if (!Object.prototype.hasOwnProperty.call(process.env, "AWS_ACCESS_KEY_ID")) {
+                            // eslint-disable-next-line no-console
+                            console.log("[ S3 ] - There is no AWS_ACCESS_KEY_ID variable in the .env file.");
+                        } else if (!Object.prototype.hasOwnProperty.call(process.env, "AWS_SECRET_ACCESS_KEY")) {
+                            // eslint-disable-next-line no-console
+                            console.log("[ S3 ] - There is no AWS_SECRET_ACCESS_KEY variable in the .env file.");
+                        } else if (!Object.prototype.hasOwnProperty.call(process.env, "AWS_DEFAULT_REGION")) {
+                            // eslint-disable-next-line no-console
+                            console.log("[ S3 ] - There is no AWS_DEFAULT_REGION variable in the .env file.");
+                        } else {
+                            _scope.$.set("s3", (0, _multer2.default)({
+                                dest: _path2.default.resolve(process.env.S3_TMP_UPLOAD_DIRETORY),
+                                storage: (0, _multerS2.default)({
+                                    s3: new _awsSdk2.default.S3(),
+                                    bucket: process.env.S3_BUCKET_NAME,
+                                    contentType: _multerS2.default.AUTO_CONTENT_TYPE,
+                                    acl: "public-read",
+                                    key: function key(req, file, cb) {
+                                        _crypto2.default.randomBytes(16, function (err, hash) {
+                                            if (err) cb(err);else {
+                                                var hashHex = hash.toString("hex");
+                                                var fileName = hashHex.substring(0, 2) + "/" + hashHex.substring(2, 5) + "/" + hashHex.substring(5, 9) + "/" + hashHex.substring(9, 14) + "/" + hashHex.substring(14, 20) + "/" + file.originalname;
+                                                cb(null, fileName);
+                                            }
+                                        });
+                                    }
+                                }),
+                                limits: {
+                                    fileSize: 5 * 1024 * 1024
+                                }
+                            }));
+                        }
+                    } catch (e) {
+                        // eslint-disable-next-line no-console
+                        console.log("[ S3 ] - " + e.message);
+                    }
+
+                case 1:
+                case "end":
+                    return _context.stop();
+            }
+        }
+    }, _callee, undefined);
+}));
+//# sourceMappingURL=index.js.map
